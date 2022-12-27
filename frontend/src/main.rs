e42eebfb9dc7e5400f243c45a3bf85c2037d5100
Scott Techau <scott.techau@gmail.com>
2022-12-27 16:42:47 -0600

    Add simple API call

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,3 +1,5 @@
+use gloo_net::http::Request;
+use wasm_bindgen_futures::spawn_local;
 use yew::prelude::*;
 use yew_router::prelude::*;
 
@@ -5,11 +7,15 @@ use yew_router::prelude::*;
 enum Route {
     #[at("/")]
     Home,
+
+    #[at("/hello-server")]
+    HelloServer,
 }
 
 fn switch(routes: Route) -> Html {
     match routes {
         Route::Home => html! { <h1>{ "Hello Frontend" }</h1> },
+        Route::HelloServer => html! { <HelloServer /> },
     }
 }
 
@@ -19,6 +25,51 @@ fn app() -> Html {
         <BrowserRouter>
             <Switch<Route> render={switch} />
         </BrowserRouter>
+    }
+}
+
+#[function_component(HelloServer)]
+fn hello_server() -> Html {
+    let data = use_state(|| None);
+
+    {
+        let data = data.clone();
+        use_effect(move || {
+            spawn_local(async move {
+                let resp = Request::get("/api/hello").send().await;
+                let result = match resp {
+                    Ok(resp) => if !resp.ok() {
+                        Err(format!(
+                            "Error fetching data {} ({})",
+                            resp.status(),
+                            resp.status_text()
+                        ))
+                    } else {
+                        resp.text().await.map_err(|err| err.to_string())
+                    },
+                    Err(err) => Err(err.to_string()),
+                };
+                data.set(Some(result));
+            })
+        });
+    }
+
+    match data.as_ref() {
+        None => {
+            html! {
+                <div>{"No server response"}</div>
+            }
+        }
+        Some(Ok(data)) => {
+            html! {
+                <div>{"Got server response: "}{data}</div>
+            }
+        }
+        Some(Err(err)) => {
+            html! {
+                <div>{"Error requesting data from server: "}{err}</div>
+            }
+        }
     }
 }
 
