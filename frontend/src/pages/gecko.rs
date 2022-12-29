6649e9a13df1d6044e32fd01a9d2098843796db5
Scott Techau <scott.techau@gmail.com>
2022-12-29 13:38:18 -0800

    Minor refactor and run clippy

--- frontend/src/pages/gecko.rs
+++ frontend/src/pages/gecko.rs
@@ -1,103 +1,108 @@
 use std::rc::Rc;
 
-use models::{Topic, TOPIC_GRID_ROWS, TOPIC_GRID_COLS};
 use gloo_net::http::Request;
-use yew::{prelude::*, platform::spawn_local};
+use models::{Topic, TOPIC_GRID_COLS, TOPIC_GRID_ROWS};
+use yew::{platform::spawn_local, prelude::*};
 
 #[function_component(GeckoPage)]
 pub fn page() -> Html {
-  let topic = use_state(|| None).clone();
+    let topic = use_state(|| None);
 
-  let topic_card = topic.as_ref().map(|res| {
-    match res {
-      Ok(topic) => html! {
-        <TopicCard topic={topic}/>
-      },
-      Err(err) => html! {
-        <div>
-        {"An error occurred fetching topic from server"}
-        {err}
-        </div>
-      },
+    let topic_card = topic.as_ref().map(|res| match res {
+        Ok(topic) => html! {
+          <TopicCard topic={topic}/>
+        },
+        Err(err) => html! {
+          <div>
+          {"An error occurred fetching topic from server"}
+          {err}
+          </div>
+        },
+    });
+
+    let new_topic = Callback::from(move |_| {
+        let topic = topic.clone();
+        spawn_local(async move {
+            topic.set(Some(fetch_topic().await));
+        });
+    });
+
+    html! {
+      <div class="Gecko">
+        { topic_card }
+        <button onclick={new_topic}>{"New topic"}</button>
+      </div>
     }
-  });
-
-  html! {
-    <div class="Gecko">
-      { topic_card }
-      <button onclick={move |_| {
-        fetch_topic(topic.setter());
-      }} >{"New topic"}</button>
-    </div>
-  }
 }
 
-fn fetch_topic(setter: UseStateSetter<Option<Result<Rc<Topic>, String>>>) {
-  spawn_local(async move {
+async fn fetch_topic() -> Result<Rc<Topic>, String> {
     let resp = Request::get("/api/gecko/random-card").send().await;
-    let result = match resp {
-      Ok(resp) => if resp.ok() {
-        resp.json::<Topic>()
-            .await
-            .map(Rc::new)
-            .map_err(|err| err.to_string())
-      } else {
-        Err(format!(
-          "Error fetching data {} ({})",
-          resp.status(),
-          resp.status_text(),
-        ))
-      }
-      Err(err) => Err(err.to_string()),
-    };
-
-    setter.set(Some(result));
-  });
+    match resp {
+        Ok(resp) => {
+            if resp.ok() {
+                resp.json::<Topic>()
+                    .await
+                    .map(Rc::new)
+                    .map_err(|err| err.to_string())
+            } else {
+                Err(format!(
+                    "Error fetching data {} ({})",
+                    resp.status(),
+                    resp.status_text(),
+                ))
+            }
+        }
+        Err(err) => Err(err.to_string()),
+    }
 }
 
 #[derive(Properties, PartialEq)]
 pub struct TopicProps {
-  topic: Rc<Topic>,
+    topic: Rc<Topic>,
 }
 
 #[function_component(TopicCard)]
 pub fn topic_card(props: &TopicProps) -> Html {
-  let topic = props.topic.as_ref();
-  
-  let word_rows = (0..TOPIC_GRID_ROWS).map(|row| {
-    let cols = (0..TOPIC_GRID_COLS).map(|col| {
-      let i = row * TOPIC_GRID_COLS + col;
-      html!{
-        <td key={col}>{&topic.words[i]}</td>
-      }
-    }).collect::<Html>();
+    let topic = props.topic.as_ref();
+
+    let word_rows = (0..TOPIC_GRID_ROWS)
+        .map(|row| {
+            let cols = (0..TOPIC_GRID_COLS)
+                .map(|col| {
+                    let i = row * TOPIC_GRID_COLS + col;
+                    html! {
+                      <td key={col}>{&topic.words[i]}</td>
+                    }
+                })
+                .collect::<Html>();
+
+            html! {
+              <tr key={row}>
+                <td>{row + 1}</td>
+                {cols}
+              </tr>
+            }
+        })
+        .collect::<Html>();
 
     html! {
-      <tr key={row}>
-        <td>{row + 1}</td>
-        {cols}
-      </tr>
+      <div class="Topic">
+        <h1>{&topic.name}</h1>
+        <table>
+          <thead>
+            <tr>
+              // TODO this can probably be a loop?
+              <th />
+              <th>{'A'}</th>
+              <th>{'B'}</th>
+              <th>{'C'}</th>
+              <th>{'D'}</th>
+            </tr>
+          </thead>
+          <tbody>
+            {word_rows}
+          </tbody>
+        </table>
+      </div>
     }
-  }).collect::<Html>();
-
-  html! {
-    <div class="Topic">
-      <h1>{&topic.name}</h1>
-      <table>
-        <thead>
-          <tr>
-            // TODO this can probably be a loop?
-            <th />
-            <th>{'A'}</th>
-            <th>{'B'}</th>
-            <th>{'C'}</th>
-            <th>{'D'}</th>
-          </tr>
-        </thead>
-        <tbody>
-          {word_rows}
-        </tbody>
-      </table>
-    </div>
-  }
 }
--- server/src/api/gecko.rs
+++ server/src/api/gecko.rs
@@ -1,17 +1,16 @@
-use axum::{Router, response::IntoResponse, routing::get, http::Response};
+use axum::{http::Response, response::IntoResponse, routing::get, Router};
 use lazy_static::lazy_static;
 use models::Topic;
-use rand::{Rng, thread_rng};
+use rand::{thread_rng, Rng};
 
 const TOPICS_RON: &str = include_str!("gecko-topics.ron");
 
 lazy_static! {
-    static ref TOPICS: Vec<Topic> = ron::from_str(&TOPICS_RON).unwrap();
+    static ref TOPICS: Vec<Topic> = ron::from_str(TOPICS_RON).unwrap();
 }
 
 pub fn gecko_api() -> Router {
-  Router::new()
-    .route("/random-card", get(random_card))
+    Router::new().route("/random-card", get(random_card))
 }
 
 async fn random_card() -> impl IntoResponse {
@@ -20,9 +19,7 @@ async fn random_card() -> impl IntoResponse {
 
     let serialized = serde_json::to_string(topic).unwrap();
 
-    Response::builder()
-        .body(serialized)
-        .unwrap()
+    Response::builder().body(serialized).unwrap()
 }
 
 fn choose_random_card<R: Rng + ?Sized>(rng: &mut R) -> &Topic {
