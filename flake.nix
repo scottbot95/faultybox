8d6de4e0070c105cf69eba0be0716bb0a03adf0f
Scott Techau <scott.techau@gmail.com>
2022-12-31 02:12:42 -0800

    Add nix package support

--- flake.nix
+++ flake.nix
@@ -10,16 +10,84 @@
   outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        overlays = [ (import rust-overlay) ];
+        overlays = [ 
+          (import rust-overlay)
+          (super: self: {
+
+          })
+        ];
         pkgs = import nixpkgs {
           inherit system overlays;
         };
+        rust-toolchain = with pkgs; [
+          (rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
+          cargo-make
+          trunk
+          wasm-bindgen-cli
+        ];
       in
       with pkgs;
-      {
+      rec {
+        packages = {
+          faultybox-frontend = mkYarnModules {
+            pname = "faultybox-frontend";
+            version = "0.1.0";
+            packageJSON = ./frontend/package.json;
+            yarnLock = ./frontend/yarn.lock;
+            yarnNix = ./frontend/yarn.nix;
+          };
+
+          faultybox = rustPlatform.buildRustPackage rec {
+            pname = "faultybox";
+            version = "0.1.0";
+
+            nativeBuildInputs = rust-toolchain ++ [
+              makeWrapper
+              sass
+            ];
+
+            src = ./.;
+
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+            };
+
+            buildPhase = ''
+              runHook preBuild
+
+              mkdir -p ./frontend/node_modules
+              cp -r ${packages.faultybox-frontend}/node_modules/\@patternfly ./frontend/node_modules
+              PATH=${packages.faultybox-frontend}/node_modules/.bin:$PATH cargo make build -j $NIX_BUILD_CORES --release
+
+              runHook postBuild
+            '';
+
+            installPhase = ''
+              runHook preInstall
+
+              # mkdir -p $out
+              # cp -r ./ $out/
+
+              mkdir -p $out/bin
+              
+              cp -v ./target/release/server $out/bin/
+              cp -rv ./dist/ $out/
+
+              wrapProgram $out/bin/server --add-flags "--static-dir $out/dist/"
+
+              runHook postInstall
+            '';
+
+            checkPase = ''
+              runHook preCheck
+              runHook postCheck
+            '';
+          };
+        };
+        defaultPackage = packages.faultybox;
         devShells.default = mkShell {
-          buildInputs = [
-            (rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
+          buildInputs = rust-toolchain ++ [
+            yarn2nix
           ];
         };
       }
---
+++ frontend/yarn.nix
@@ -0,0 +1,157 @@
+{ fetchurl, fetchgit, linkFarm, runCommand, gnutar }: rec {
+  offline_cache = linkFarm "offline" packages;
+  packages = [
+    {
+      name = "https___registry.npmjs.org__patternfly_patternfly___patternfly_4.222.4.tgz";
+      path = fetchurl {
+        name = "https___registry.npmjs.org__patternfly_patternfly___patternfly_4.222.4.tgz";
+        url  = "https://registry.npmjs.org/@patternfly/patternfly/-/patternfly-4.222.4.tgz";
+        sha512 = "+0fk4dzxEbWn+hgaOnR0BjfeUdEeVVrqfH7+GFeFc+RKjEMjIR/BZbWWN8YQDezmDv6A32gHmEKaY3Uwbt06Lw==";
+      };
+    }
+    {
+      name = "https___registry.npmjs.org_anymatch___anymatch_3.1.3.tgz";
+      path = fetchurl {
+        name = "https___registry.npmjs.org_anymatch___anymatch_3.1.3.tgz";
+        url  = "https://registry.npmjs.org/anymatch/-/anymatch-3.1.3.tgz";
+        sha512 = "KMReFUr0B4t+D+OBkjR3KYqvocp2XaSzO55UcB6mgQMd3KbcE+mWTyvVV7D/zsdEbNnV6acZUutkiHQXvTr1Rw==";
+      };
+    }
+    {
+      name = "https___registry.npmjs.org_binary_extensions___binary_extensions_2.2.0.tgz";
+      path = fetchurl {
+        name = "https___registry.npmjs.org_binary_extensions___binary_extensions_2.2.0.tgz";
+        url  = "https://registry.npmjs.org/binary-extensions/-/binary-extensions-2.2.0.tgz";
+        sha512 = "jDctJ/IVQbZoJykoeHbhXpOlNBqGNcwXJKJog42E5HDPUwQTSdjCHdihjj0DlnheQ7blbT6dHOafNAiS8ooQKA==";
+      };
+    }
+    {
+      name = "https___registry.npmjs.org_braces___braces_3.0.2.tgz";
+      path = fetchurl {
+        name = "https___registry.npmjs.org_braces___braces_3.0.2.tgz";
+        url  = "https://registry.npmjs.org/braces/-/braces-3.0.2.tgz";
+        sha512 = "b8um+L1RzM3WDSzvhm6gIz1yfTbBt6YTlcEKAvsmqCZZFw46z626lVj9j1yEPW33H5H+lBQpZMP1k8l+78Ha0A==";
+      };
+    }
+    {
+      name = "https___registry.npmjs.org_chokidar___chokidar_3.5.3.tgz";
+      path = fetchurl {
+        name = "https___registry.npmjs.org_chokidar___chokidar_3.5.3.tgz";
+        url  = "https://registry.npmjs.org/chokidar/-/chokidar-3.5.3.tgz";
+        sha512 = "Dr3sfKRP6oTcjf2JmUmFJfeVMvXBdegxB0iVQ5eb2V10uFJUCAS8OByZdVAyVb8xXNz3GjjTgj9kLWsZTqE6kw==";
+      };
+    }
+    {
+      name = "https___registry.npmjs.org_fill_range___fill_range_7.0.1.tgz";
+      path = fetchurl {
+        name = "https___registry.npmjs.org_fill_range___fill_range_7.0.1.tgz";
+        url  = "https://registry.npmjs.org/fill-range/-/fill-range-7.0.1.tgz";
+        sha512 = "qOo9F+dMUmC2Lcb4BbVvnKJxTPjCm+RRpe4gDuGrzkL7mEVl/djYSu2OdQ2Pa302N4oqkSg9ir6jaLWJ2USVpQ==";
+      };
+    }
+    {
+      name = "fsevents___fsevents_2.3.2.tgz";
+      path = fetchurl {
+        name = "fsevents___fsevents_2.3.2.tgz";
+        url  = "https://registry.yarnpkg.com/fsevents/-/fsevents-2.3.2.tgz";
+        sha512 = "xiqMQR4xAeHTuB9uWm+fFRcIOgKBMiOBP+eXiyT7jsgVCq1bkVygt00oASowB7EdtpOHaaPgKt812P9ab+DDKA==";
+      };
+    }
+    {
+      name = "https___registry.npmjs.org_glob_parent___glob_parent_5.1.2.tgz";
+      path = fetchurl {
+        name = "https___registry.npmjs.org_glob_parent___glob_parent_5.1.2.tgz";
+        url  = "https://registry.npmjs.org/glob-parent/-/glob-parent-5.1.2.tgz";
+        sha512 = "AOIgSQCepiJYwP3ARnGx+5VnTu2HBYdzbGP45eLw1vr3zB3vZLeyed1sC9hnbcOc9/SrMyM5RPQrkGz4aS9Zow==";
+      };
+    }
+    {
+      name = "https___registry.npmjs.org_immutable___immutable_4.2.1.tgz";
+      path = fetchurl {
+        name = "https___registry.npmjs.org_immutable___immutable_4.2.1.tgz";
+        url  = "https://registry.npmjs.org/immutable/-/immutable-4.2.1.tgz";
+        sha512 = "7WYV7Q5BTs0nlQm7tl92rDYYoyELLKHoDMBKhrxEoiV4mrfVdRz8hzPiYOzH7yWjzoVEamxRuAqhxL2PLRwZYQ==";
+      };
+    }
+    {
+      name = "https___registry.npmjs.org_is_binary_path___is_binary_path_2.1.0.tgz";
+      path = fetchurl {
+        name = "https___registry.npmjs.org_is_binary_path___is_binary_path_2.1.0.tgz";
+        url  = "https://registry.npmjs.org/is-binary-path/-/is-binary-path-2.1.0.tgz";
+        sha512 = "ZMERYes6pDydyuGidse7OsHxtbI7WVeUEozgR/g7rd0xUimYNlvZRE/K2MgZTjWy725IfelLeVcEM97mmtRGXw==";
+      };
+    }
+    {
+      name = "https___registry.npmjs.org_is_extglob___is_extglob_2.1.1.tgz";
+      path = fetchurl {
+        name = "https___registry.npmjs.org_is_extglob___is_extglob_2.1.1.tgz";
+        url  = "https://registry.npmjs.org/is-extglob/-/is-extglob-2.1.1.tgz";
+        sha512 = "SbKbANkN603Vi4jEZv49LeVJMn4yGwsbzZworEoyEiutsN3nJYdbO36zfhGJ6QEDpOZIFkDtnq5JRxmvl3jsoQ==";
+      };
+    }
+    {
+      name = "https___registry.npmjs.org_is_glob___is_glob_4.0.3.tgz";
+      path = fetchurl {
+        name = "https___registry.npmjs.org_is_glob___is_glob_4.0.3.tgz";
+        url  = "https://registry.npmjs.org/is-glob/-/is-glob-4.0.3.tgz";
+        sha512 = "xelSayHH36ZgE7ZWhli7pW34hNbNl8Ojv5KVmkJD4hBdD3th8Tfk9vYasLM+mXWOZhFkgZfxhLSnrwRr4elSSg==";
+      };
+    }
+    {
+      name = "https___registry.npmjs.org_is_number___is_number_7.0.0.tgz";
+      path = fetchurl {
+        name = "https___registry.npmjs.org_is_number___is_number_7.0.0.tgz";
+        url  = "https://registry.npmjs.org/is-number/-/is-number-7.0.0.tgz";
+        sha512 = "41Cifkg6e8TylSpdtTpeLVMqvSBEVzTttHvERD741+pnZ8ANv0004MRL43QKPDlK9cGvNp6NZWZUBlbGXYxxng==";
+      };
+    }
+    {
+      name = "https___registry.npmjs.org_normalize_path___normalize_path_3.0.0.tgz";
+      path = fetchurl {
+        name = "https___registry.npmjs.org_normalize_path___normalize_path_3.0.0.tgz";
+        url  = "https://registry.npmjs.org/normalize-path/-/normalize-path-3.0.0.tgz";
+        sha512 = "6eZs5Ls3WtCisHWp9S2GUy8dqkpGi4BVSz3GaqiE6ezub0512ESztXUwUB6C6IKbQkY2Pnb/mD4WYojCRwcwLA==";
+      };
+    }
+    {
+      name = "https___registry.npmjs.org_picomatch___picomatch_2.3.1.tgz";
+      path = fetchurl {
+        name = "https___registry.npmjs.org_picomatch___picomatch_2.3.1.tgz";
+        url  = "https://registry.npmjs.org/picomatch/-/picomatch-2.3.1.tgz";
+        sha512 = "JU3teHTNjmE2VCGFzuY8EXzCDVwEqB2a8fsIvwaStHhAWJEeVd1o1QD80CU6+ZdEXXSLbSsuLwJjkCBWqRQUVA==";
+      };
+    }
+    {
+      name = "https___registry.npmjs.org_readdirp___readdirp_3.6.0.tgz";
+      path = fetchurl {
+        name = "https___registry.npmjs.org_readdirp___readdirp_3.6.0.tgz";
+        url  = "https://registry.npmjs.org/readdirp/-/readdirp-3.6.0.tgz";
+        sha512 = "hOS089on8RduqdbhvQ5Z37A0ESjsqz6qnRcffsMU3495FuTdqSm+7bhJ29JvIOsBDEEnan5DPu9t3To9VRlMzA==";
+      };
+    }
+    {
+      name = "https___registry.npmjs.org_sass___sass_1.57.1.tgz";
+      path = fetchurl {
+        name = "https___registry.npmjs.org_sass___sass_1.57.1.tgz";
+        url  = "https://registry.npmjs.org/sass/-/sass-1.57.1.tgz";
+        sha512 = "O2+LwLS79op7GI0xZ8fqzF7X2m/m8WFfI02dHOdsK5R2ECeS5F62zrwg/relM1rjSLy7Vd/DiMNIvPrQGsA0jw==";
+      };
+    }
+    {
+      name = "https___registry.npmjs.org_source_map_js___source_map_js_1.0.2.tgz";
+      path = fetchurl {
+        name = "https___registry.npmjs.org_source_map_js___source_map_js_1.0.2.tgz";
+        url  = "https://registry.npmjs.org/source-map-js/-/source-map-js-1.0.2.tgz";
+        sha512 = "R0XvVJ9WusLiqTCEiGCmICCMplcCkIwwR11mOSD9CR5u+IXYdiseeEuXCVAjS54zqwkLcPNnmU4OeJ6tUrWhDw==";
+      };
+    }
+    {
+      name = "https___registry.npmjs.org_to_regex_range___to_regex_range_5.0.1.tgz";
+      path = fetchurl {
+        name = "https___registry.npmjs.org_to_regex_range___to_regex_range_5.0.1.tgz";
+        url  = "https://registry.npmjs.org/to-regex-range/-/to-regex-range-5.0.1.tgz";
+        sha512 = "65P7iz6X5yEr1cwcgvQxbbIw7Uk3gOy5dIdtZ4rDveLqhrdJP+Li/Hx6tyK0NEb+2GCyneCMJiGqrADCSNk8sQ==";
+      };
+    }
+  ];
+}
