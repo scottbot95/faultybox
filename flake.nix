0d3733bf484b309bd38d4814d4dead58c05bbe00
Scott Techau <scott.techau@gmail.com>
2023-01-20 13:01:20 -0800

    Refactor build process to use crane and buildNpmPackage

--- flake.nix
+++ flake.nix
@@ -1,99 +1,175 @@
 {
-  description = "Gecko: an online game written in rust";
+  description = "Build a cargo project";
 
   inputs = {
-    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-22.11";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url  = "github:numtide/flake-utils";
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    advisory-db = {
+      url = "github:rustsec/advisory-db";
       flake = false;
+    };
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
     };
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+  outputs = { self, nixpkgs, crane, flake-utils, advisory-db, rust-overlay, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        overlays = [ 
-          (import rust-overlay)
-          (super: self: {
-
-          })
-        ];
         pkgs = import nixpkgs {
-          inherit system overlays;
+          inherit system;
+          overlays = [ (import rust-overlay) ];
         };
-        rust-toolchain = with pkgs; [
-          (rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
+
+        inherit (pkgs) lib;
+
+        node_modules = pkgs.buildNpmPackage rec {
+          pname = "faultybox-frontend";
+          version = "0.1.0";
+          src = lib.cleanSourceWith {
+            src = ./frontend;
+            filter = path: _type:
+              (builtins.match ".*/frontend/package.*json" path) != null;
+          };
+
+          dontNpmBuild = true;
+
+          npmDepsHash = "sha256-LMGggPsm3AgKRscSC1sl2dLi8TT0dHW4VmmU4HB/x/Y=";
+        };
+
+        rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+
+        craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+        src = let
+          customFilter = path: _type: builtins.match ".*(ron|html|scss)$" path != null;
+          customOrCargo = path: type:
+            (customFilter path type) || (craneLib.filterCargoSources path type);
+        in lib.cleanSourceWith {
+          src = ./.;
+          filter = customOrCargo;
+        };
+
+        nativeBuildInputs = with pkgs; [
+          makeWrapper
           cargo-make
           trunk
           wasm-bindgen-cli
         ];
+
+        buildInputs = [
+          # Add additional build inputs here
+        ] ++ lib.optionals pkgs.stdenv.isDarwin [
+          # Additional darwin specific inputs can be set here
+          pkgs.libiconv
+        ];
+
+        # Build *just* the cargo dependencies, so we can reuse
+        # all of that work (e.g. via cachix) when running in CI
+        cargoArtifacts = craneLib.buildDepsOnly {
+          inherit src buildInputs;
+        };
+
+        faultybox-frontend = craneLib.buildPackage {
+          inherit cargoArtifacts src buildInputs nativeBuildInputs;
+
+          postPatch = ''
+            cp -r ${node_modules}/lib/node_modules/faultybox/node_modules/\@patternfly ./frontend/node_modules
+          '';
+
+          buildPhaseCargoCommand = ''
+            export PATH=${node_modules}/lib/node_modules/faultybox/node_modules/.bin:$PATH 
+            cargo make build -j $NIX_BUILD_CORES --release
+          '';
+
+          installPhaseCommand = ''
+            mkdir -p $out/bin
+            
+            cp -v ./target/release/server $out/bin/
+            cp -rv ./dist/ $out/
+
+            wrapProgram $out/bin/server --add-flags "--static-dir $out/dist/"
+          '';
+        };
+
+        # Build the actual crate itself, reusing the dependency
+        # artifacts from above.
+        faultybox = craneLib.buildPackage {
+          inherit cargoArtifacts src buildInputs nativeBuildInputs;
+
+          # cargoBuildCommand = "cargo make build -j $NIX_BUILD_CORES --release";
+          cargoExtraArgs = "--bin server";
+        };
       in
-      with pkgs;
-      rec {
-        packages = {
-          faultybox-frontend = mkYarnModules {
-            pname = "faultybox-frontend";
-            version = "0.1.0";
-            packageJSON = ./frontend/package.json;
-            yarnLock = ./frontend/yarn.lock;
-            yarnNix = ./frontend/yarn.nix;
+      {
+        checks = {
+          # Build the crate as part of `nix flake check` for convenience
+          inherit faultybox;
+
+          # Run clippy (and deny all warnings) on the crate source,
+          # again, resuing the dependency artifacts from above.
+          #
+          # Note that this is done as a separate derivation so that
+          # we can block the CI if there are issues here, but not
+          # prevent downstream consumers from building our crate by itself.
+          faultybox-clippy = craneLib.cargoClippy {
+            inherit cargoArtifacts src buildInputs;
+            cargoClippyExtraArgs = "--all-targets -- --deny warnings";
           };
 
-          faultybox = rustPlatform.buildRustPackage rec {
-            pname = "faultybox";
-            version = "0.1.0";
+          faultybox-doc = craneLib.cargoDoc {
+            inherit cargoArtifacts src buildInputs;
+          };
 
-            nativeBuildInputs = rust-toolchain ++ [
-              makeWrapper
-              sass
-            ];
+          # Check formatting
+          faultybox-fmt = craneLib.cargoFmt {
+            inherit src;
+          };
 
-            src = ./.;
+          # Audit dependencies
+          faultybox-audit = craneLib.cargoAudit {
+            inherit src advisory-db;
+          };
 
-            cargoLock = {
-              lockFile = ./Cargo.lock;
-            };
-
-            buildPhase = ''
-              runHook preBuild
-
-              mkdir -p ./frontend/node_modules
-              cp -r ${packages.faultybox-frontend}/node_modules/\@patternfly ./frontend/node_modules
-              PATH=${packages.faultybox-frontend}/node_modules/.bin:$PATH cargo make build -j $NIX_BUILD_CORES --release
-
-              runHook postBuild
-            '';
-
-            installPhase = ''
-              runHook preInstall
-
-              # mkdir -p $out
-              # cp -r ./ $out/
-
-              mkdir -p $out/bin
-              
-              cp -v ./target/release/server $out/bin/
-              cp -rv ./dist/ $out/
-
-              wrapProgram $out/bin/server --add-flags "--static-dir $out/dist/"
-
-              runHook postInstall
-            '';
-
-            checkPase = ''
-              runHook preCheck
-              runHook postCheck
-            '';
+          # Run tests with cargo-nextest
+          # Consider setting `doCheck = false` on `faultybox` if you do not want
+          # the tests to run twice
+          faultybox-nextest = craneLib.cargoNextest {
+            inherit cargoArtifacts src buildInputs;
+            partitions = 1;
+            partitionType = "count";
+          };
+        } // lib.optionalAttrs (system == "x86_64-linux") {
+          # NB: cargo-tarpaulin only supports x86_64 systems
+          # Check code coverage (note: this will not upload coverage anywhere)
+          faultybox-coverage = craneLib.cargoTarpaulin {
+            inherit cargoArtifacts src;
           };
         };
-        defaultPackage = packages.faultybox;
-        devShells.default = mkShell {
-          buildInputs = rust-toolchain ++ [
-            yarn2nix
-          ];
+
+        packages.default = faultybox;
+        packages.frontend = faultybox-frontend;
+
+        apps.default = flake-utils.lib.mkApp {
+          drv = faultybox;
         };
-      }
-    );
+
+        devShells.default = pkgs.mkShell {
+          inputsFrom = builtins.attrValues self.checks;
+
+          # Extra inputs can be added here
+          inherit nativeBuildInputs;
+        };
+      });
 }
